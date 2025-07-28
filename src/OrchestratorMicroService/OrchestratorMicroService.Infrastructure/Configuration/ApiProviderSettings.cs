e4dadbeb5b584c35643bf9c72b2e68263c9d6c0a
Cristian Isaac Vargas Garcia <civargas34@gmail.com>
2025-07-27 23:42:30 -0400

    feat: add infrastructure for exchange rate providers and HTTP client registration

---
+++ src/OrchestratorMicroService/OrchestratorMicroService.Infrastructure/Configuration/ApiProviderSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace OrchestratorMicroService.Infrastructure.Configuration
+{
+    public class ApiProviderSettings
+    {
+        public string Api1BaseUrl { get; set; } = string.Empty;
+        public string Api2BaseUrl { get; set; } = string.Empty;
+        public string Api3BaseUrl { get; set; } = string.Empty;
+        public string ApiExternalBaseUrl { get; set; } = string.Empty;
+    }
+}
---
+++ src/OrchestratorMicroService/OrchestratorMicroService.Infrastructure/Providers/Api1ExchangeRateProvider.cs
@@ -0,0 +1,13 @@
+﻿using OrchestratorMicroService.Application.Interfaces;
+using OrchestratorMicroService.Domain.Models;
+
+namespace OrchestratorMicroService.Infrastructure.Providers
+{
+    public class Api1ExchangeRateProvider : IExchangeRateProvider
+    {
+        public Task<CurrencyResult> GetExchangeRateAsync(CurrencyRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
